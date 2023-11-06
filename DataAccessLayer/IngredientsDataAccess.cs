37276bfa53fc22c3d183060c06880b0502aa74ac
codebeautyprogramming <codebeautyprogramming@gmail.com>
2023-11-06 14:36:47 +0100

    Startup code for this chapter

---
+++ DataAccessLayer/IngredientsDataAccess.cs
@@ -0,0 +1,17 @@
+﻿using DomainModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class IngredientsDataAccess
+    {
+        public void AddIngredient(Ingredient ingredient)
+        {
+
+        }
+    }
+}
